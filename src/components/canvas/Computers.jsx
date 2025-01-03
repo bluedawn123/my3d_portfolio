7830bc5bc8997f49e015e38e809f676baa230f13
bluedawn123 <haemilyjh@naver.com>
2025-01-03 19:37:50 +0900

    컴퓨터 거리 조절, 별배경 넣기(근데 무거워짐... 추후 고민해보기)

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -31,8 +31,8 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 1.5 : 4}
+        position={isMobile ? [0, -3, -2.2] : [0, -1, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -68,7 +68,7 @@ const ComputersCanvas = () => {
       frameloop="always"
       shadows
       dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{ position: [20, 3, 6], fov: 25 }} // 기존 [20, 3, 5] 수정
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
@@ -86,3 +86,93 @@ const ComputersCanvas = () => {
 };
 
 export default ComputersCanvas;
+
+
+// import React, { Suspense, useEffect, useRef, useState } from "react";
+// import { Canvas, useFrame } from "@react-three/fiber";
+// import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+// import CanvasLoader from "../Loader";
+
+// const Computers = ({ isMobile }) => {
+//   const computer = useGLTF("./desktop_pc/scene.gltf");
+
+//   // Ref를 사용하여 mesh를 참조
+//   const computerRef = useRef();
+
+//   // useFrame을 사용해 매 프레임마다 회전 업데이트
+//   useFrame(() => {
+//     if (computerRef.current) {
+//       computerRef.current.rotation.y += 0.002; // 천천히 회전
+//     }
+//   });
+
+//   return (
+//     <mesh ref={computerRef}>
+//       <hemisphereLight intensity={0.15} groundColor="black" />
+//       <spotLight
+//         position={[-20, 50, 10]}
+//         angle={0.12}
+//         penumbra={1}
+//         intensity={1}
+//         castShadow
+//         shadow-mapSize={1024}
+//       />
+//       <pointLight intensity={1} />
+//       <primitive
+//         object={computer.scene}
+//         scale={isMobile ? 0.7 : 0.75}
+//         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+//         rotation={[-0.01, -0.2, -0.1]}
+//       />
+//     </mesh>
+//   );
+// };
+
+// const ComputersCanvas = () => {
+//   const [isMobile, setIsMobile] = useState(false);
+
+//   useEffect(() => {
+//     // Add a listener for changes to the screen size
+//     const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+//     // Set the initial value of the `isMobile` state variable
+//     setIsMobile(mediaQuery.matches);
+
+//     // Define a callback function to handle changes to the media query
+//     const handleMediaQueryChange = (event) => {
+//       setIsMobile(event.matches);
+//     };
+
+//     // Add the callback function as a listener for changes to the media query
+//     mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+//     // Remove the listener when the component is unmounted
+//     return () => {
+//       mediaQuery.removeEventListener("change", handleMediaQueryChange);
+//     };
+//   }, []);
+
+//   return (
+//     <Canvas
+//       frameloop="always"
+//       shadows
+//       dpr={[1, 2]}
+//       camera={{ position: [20, 3, 5], fov: 25 }}
+//       gl={{ preserveDrawingBuffer: true }}
+//     >
+//       <Suspense fallback={<CanvasLoader />}>
+//         <OrbitControls
+//           enableZoom={false}
+//           maxPolarAngle={Math.PI / 2}
+//           minPolarAngle={Math.PI / 2}
+//         />
+//         <Computers isMobile={isMobile} />
+//       </Suspense>
+
+//       <Preload all />
+//     </Canvas>
+//   );
+// };
+
+// export default ComputersCanvas;
